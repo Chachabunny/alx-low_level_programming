7c87531831964e1441b94141ddbe77459332acb3
Chachabunny <Chachabunny@gmail.com>
2023-06-15 12:31:47 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program prints the message "with proper grammar, but the outcome is a piece of art,"
+ * using the printf function and returns 0 to indicate successful execution.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
