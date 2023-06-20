3892e84fb0ff8c20e1f4792ad0ddce1d66c31d49
Chachabunny <Chachabunny@gmail.com>
2023-06-20 10:39:58 -0700

    hello

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function executes the main logic of the program.
+ * It returns an integer value to indicate the program's exit status.
+ *
+ * Return: 0 on successful execution
+ */
 
-int main(void) {
+int main(void)
+{
 	printf("_putchar\n");
-	return 0;
+	return (0);
 }
