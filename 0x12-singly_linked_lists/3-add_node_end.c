570c540b5155ed6fc9d18a142c343d818b892ea5
Chchabunny <charitymukabane@gmail.com>
2023-07-27 12:19:12 -0700

    task

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (!new)
-	return (NULL);
+		return (NULL);
 
 	new->str = strdup(str);
 	new->len = len;
@@ -33,9 +33,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	while (temp->next)
-		temp = temp->next;
+	temp = temp->next;
 
-		temp->next = new;
+	temp->next = new;
 
 	return (new);
 }
